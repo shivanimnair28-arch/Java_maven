dd0d22611a7ff959be789fbb19a18ee241e4cbdf
shivanimnair28-arch <shivanimnair28@gmail.com>
2026-04-12 21:40:47 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,22 +1,9 @@
-public class App {
+ublic class App {
+ public static void main(String[] args) throws Exception {
+ System.out.println("Java CI/CD Application Started...");
+ while (true) {
+ Thread.sleep(5000);
+ System.out.println("Hello from Java CI/CD Pipeline!");
+ }
+ }
 
-public static void main(String[] args) throws Exception {
-
-System.out.println("Java CI/CD Application Started...");
-//commit 
-if (args.length > 0) {
-    System.out.println("Received argument: " + args[0]);
-    if(args[0]%2==0)
-    {
-        System.out.println("Even number received: " + args[0]);
-    }
-    else
-    {
-        System.out.println("Odd number received: " + args[0]);
-    }
-} else {
-    System.out.println("No arguments received.");
-
-}
-
-}
